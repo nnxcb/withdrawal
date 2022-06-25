1264e4094bdb91c3a0da273c29e0aba13d680113
nnxcb <984130215@qq.com>
2022-06-25 22:45:47 +0800

    修改正则

--- src/components/input.ts
+++ src/components/input.ts
@@ -11,24 +11,9 @@ export default class Inputcomp {
     this.inputVal = val;
   }
   inputKey(e:number | string) {
-    let text: string = ''
-    if (e === '.') {
-      if (!this.inputVal) { // 首个字符串为小数点时前面添加一个0
-        text = '0.'
-      } else if (this.inputVal.includes('.')) { // 有小数点不能再次输入小数点
-        return;
-      } else {
-        text = `${this.inputVal}${e}`
-      }
-    } else {
-      const index = this.inputVal.indexOf('.'); // 获取小数点在数字中的位置
-      if (index > -1 && this.inputVal.length - 1 - index >= 2) { // 最多展示小数点后两位
-        return;
-      }
-      text = `${this.inputVal}${e}`
-    }
-    const list = text.split('.');
-    text = `${Number(list[0])}${list.length === 2 ? `.${list[1]}` : ''}` // 去除字符串前多余的0及保留最后一位小数点
-    this.inputVal = text;
+    let l = `${this.inputVal}${e}`;
+    const reg1 = /0*([1-9]\d*|0\.\d+)/;
+    l = l.replace(reg1, '$1').replace(".","$#$").replace(/\./g,"").replace("$#$",".").replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3'); 
+    this.inputVal = l === '.' ? '0.' : l;
   }
 }
